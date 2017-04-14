bce104637c4866c30935dc4699f5fd64d41320c5
pirlo-san <28638322@qq.com>
2017-04-14 22:35:53 +0800

    Dijkstra test, 2nd case

--- algorithm/graph_test.c
+++ algorithm/graph_test.c
@@ -95,7 +95,7 @@ static void _graph_test_case01(void)
 static void _graph_test_case02(void)
 {
 #define vindex(c) ((c) - 'A')
-    
+
     const char  vertices[]   = {'A', 'B', 'C', 'D', 'E', 'F'};
     int         i            = 0;
     int         graphid      = -1;
@@ -225,10 +225,262 @@ static void _graph_test_case02(void)
     ASSERT_EQUAL_INT(e_graph_success, graph_free(graphid));
 }
 
+/* Dijkstra, 2 graphs */
+static void _graph_test_case03(void)
+{
+#define vindex1(c) ((c) - 'A')
+#define vindex2(v) ((v) - 1)
+
+
+    int        graphid1      = -1;
+    int        graphid2      = -1;
+    const char vertices1[]   = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int        vertices2[]   = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int        nvertex       = 0;
+    int        nedge         = 0;   
+    int        indegree      = 0;
+    int        outdegree     = 0;
+    int        total_degree1 = 0;  
+    int        weight        = 0;
+    int        distances[10] = {0};
+    int        prevs[10]     = {0};    
+    int        i             = 0;    
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_alloc(0, &graphid1));
+    ASSERT_EQUAL_INT(e_graph_success, graph_alloc(0, &graphid2));
+
+    for (i = 0; i < arrsize(vertices1); ++i)
+        ASSERT_EQUAL_INT(e_graph_success, graph_add_vertex(graphid1));
+
+    for (i = 0; i < arrsize(vertices2); ++i)
+        ASSERT_EQUAL_INT(e_graph_success, graph_add_vertex(graphid2));    
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_vertex_num(graphid1, &nvertex));
+    ASSERT_EQUAL_INT(arrsize(vertices1), nvertex);
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_vertex_num(graphid2, &nvertex));
+    ASSERT_EQUAL_INT(arrsize(vertices2), nvertex);    
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_add_edge(graphid1, vindex1('A'), vindex1('B'), 96));
+    ASSERT_EQUAL_INT(e_graph_success, graph_add_edge(graphid1, vindex1('A'), vindex1('C'), 88));
+    ASSERT_EQUAL_INT(e_graph_success, graph_add_edge(graphid1, vindex1('A'), vindex1('D'), 268));
+
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('B'), vindex1('A'), 2));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('B'), vindex1('C'), 66));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('B'), vindex1('E'), 88));
+
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('C'), vindex1('A'), 88));
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('C'), vindex1('C'), 88));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('C'), vindex1('D'), 150));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('C'), vindex1('F'), 90));
+
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('D'), vindex1('A'), 88));
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('D'), vindex1('C'), 88));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('D'), vindex1('F'), 30));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('D'), vindex1('G'), 80));    
+
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('E'), vindex1('B'), 2));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('E'), vindex1('F'), 60));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('E'), vindex1('I'), 80));  
+
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('F'), vindex1('C'), 88));  
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('F'), vindex1('D'), 88));  
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('F'), vindex1('E'), 88));  
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('F'), vindex1('G'), 130));  
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('F'), vindex1('H'), 65));  
+
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('G'), vindex1('D'), 88));
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('G'), vindex1('F'), 88));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('G'), vindex1('H'), 50));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('G'), vindex1('J'), 150));       
+
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('H'), vindex1('F'), 88));
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('H'), vindex1('G'), 88));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('H'), vindex1('I'), 120));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('H'), vindex1('J'), 80)); 
+
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('I'), vindex1('E'), 2));
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('I'), vindex1('H'), 60));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid1, vindex1('I'), vindex1('J'), 120));  
+
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('J'), vindex1('G'), 2));
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('J'), vindex1('H'), 60));
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid1, vindex1('J'), vindex1('I'), 88));      
+
+
+
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(1), vindex2(2), 6));      
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(1), vindex2(3), 4));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(1), vindex2(4), 5));
+    
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(2), vindex2(1), 1));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(2), vindex2(5), 1));
+    
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(3), vindex2(1), 1));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(3), vindex2(5), 1));
+
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(4), vindex2(1), 1));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(4), vindex2(6), 2));
+    
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(5), vindex2(2), 1));
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(5), vindex2(3), 1));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(5), vindex2(7), 9));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(5), vindex2(8), 7));
+    
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(6), vindex2(4), 1));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(6), vindex2(8), 4));
+    
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(7), vindex2(5), 1));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(7), vindex2(9), 2));
+    
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(8), vindex2(5), 1));
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(8), vindex2(6), 1));
+    ASSERT_EQUAL_INT(e_graph_success,             graph_add_edge(graphid2, vindex2(8), vindex2(9), 4));
+    
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(9), vindex2(7), 1));
+    ASSERT_EQUAL_INT(e_graph_err_edge_duplicated, graph_add_edge(graphid2, vindex2(9), vindex2(8), 1));
+
+        
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_num(graphid1, &nedge));
+    ASSERT_EQUAL_INT(18, nedge);
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_num(graphid2, &nedge));
+    ASSERT_EQUAL_INT(11, nedge);    
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid1, vindex1('C'), vindex1('F'), &weight));
+    ASSERT_EQUAL_INT(90, weight);
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid1, vindex1('F'), vindex1('C'), &weight));
+    ASSERT_EQUAL_INT(90, weight);    
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid1, vindex1('H'), vindex1('G'), &weight));
+    ASSERT_EQUAL_INT(50, weight); 
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid1, vindex1('G'), vindex1('H'), &weight));
+    ASSERT_EQUAL_INT(50, weight);    
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid1, vindex1('A'), vindex1('G'), &weight));
+    ASSERT_EQUAL_INT(M_GRAPH_INFINITE_WEIGHT, weight); 
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid1, vindex1('G'), vindex1('A'), &weight));
+    ASSERT_EQUAL_INT(M_GRAPH_INFINITE_WEIGHT, weight);   
+
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid2, vindex2(1), vindex2(2), &weight));
+    ASSERT_EQUAL_INT(6, weight); 
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid2, vindex2(2), vindex2(1), &weight));
+    ASSERT_EQUAL_INT(6, weight);   
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid2, vindex2(8), vindex2(9), &weight));
+    ASSERT_EQUAL_INT(4, weight); 
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid2, vindex2(9), vindex2(8), &weight));
+    ASSERT_EQUAL_INT(4, weight);    
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid2, vindex2(1), vindex2(9), &weight));
+    ASSERT_EQUAL_INT(M_GRAPH_INFINITE_WEIGHT, weight); 
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_get_edge_weight(graphid2, vindex2(9), vindex2(1), &weight));
+    ASSERT_EQUAL_INT(M_GRAPH_INFINITE_WEIGHT, weight);     
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_dijkstra(graphid1, vindex1('A'), distances, prevs));
+    ASSERT_EQUAL_INT(0,  distances[vindex1('A')]);
+    ASSERT_EQUAL_INT(96, distances[vindex1('B')]);
+    ASSERT_EQUAL_INT(88, distances[vindex1('C')]);
+    ASSERT_EQUAL_INT(208, distances[vindex1('D')]);
+    ASSERT_EQUAL_INT(184, distances[vindex1('E')]);
+    ASSERT_EQUAL_INT(178, distances[vindex1('F')]);
+    ASSERT_EQUAL_INT(288, distances[vindex1('G')]);
+    ASSERT_EQUAL_INT(243, distances[vindex1('H')]);
+    ASSERT_EQUAL_INT(264, distances[vindex1('I')]);
+    ASSERT_EQUAL_INT(323, distances[vindex1('J')]);
+
+    ASSERT_EQUAL_INT(vindex1('A'), prevs[vindex1('A')]);
+    ASSERT_EQUAL_INT(vindex1('A'), prevs[vindex1('B')]);
+    ASSERT_EQUAL_INT(vindex1('A'), prevs[vindex1('C')]);
+    ASSERT_EQUAL_INT(vindex1('F'), prevs[vindex1('D')]);
+    ASSERT_EQUAL_INT(vindex1('B'), prevs[vindex1('E')]);
+    ASSERT_EQUAL_INT(vindex1('C'), prevs[vindex1('F')]);
+    ASSERT_EQUAL_INT(vindex1('D'), prevs[vindex1('G')]);
+    ASSERT_EQUAL_INT(vindex1('F'), prevs[vindex1('H')]);
+    ASSERT_EQUAL_INT(vindex1('E'), prevs[vindex1('I')]);
+    ASSERT_EQUAL_INT(vindex1('H'), prevs[vindex1('J')]);
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_dijkstra(graphid1, vindex1('F'), distances, prevs));
+    ASSERT_EQUAL_INT(178,  distances[vindex1('A')]);
+    ASSERT_EQUAL_INT(148, distances[vindex1('B')]);
+    ASSERT_EQUAL_INT(90, distances[vindex1('C')]);
+    ASSERT_EQUAL_INT(30, distances[vindex1('D')]);
+    ASSERT_EQUAL_INT(60, distances[vindex1('E')]);
+    ASSERT_EQUAL_INT(0, distances[vindex1('F')]);
+    ASSERT_EQUAL_INT(110, distances[vindex1('G')]);
+    ASSERT_EQUAL_INT(65, distances[vindex1('H')]);
+    ASSERT_EQUAL_INT(140, distances[vindex1('I')]);
+    ASSERT_EQUAL_INT(145, distances[vindex1('J')]);
+
+    ASSERT_EQUAL_INT(vindex1('C'), prevs[vindex1('A')]);
+    ASSERT_EQUAL_INT(vindex1('E'), prevs[vindex1('B')]);
+    ASSERT_EQUAL_INT(vindex1('F'), prevs[vindex1('C')]);
+    ASSERT_EQUAL_INT(vindex1('F'), prevs[vindex1('D')]);
+    ASSERT_EQUAL_INT(vindex1('F'), prevs[vindex1('E')]);
+    ASSERT_EQUAL_INT(vindex1('F'), prevs[vindex1('F')]);
+    ASSERT_EQUAL_INT(vindex1('D'), prevs[vindex1('G')]);
+    ASSERT_EQUAL_INT(vindex1('F'), prevs[vindex1('H')]);
+    ASSERT_EQUAL_INT(vindex1('E'), prevs[vindex1('I')]);
+    ASSERT_EQUAL_INT(vindex1('H'), prevs[vindex1('J')]);    
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_dijkstra(graphid2, vindex2(9), distances, prevs));
+    ASSERT_EQUAL_INT(15,  distances[vindex2(1)]);
+    ASSERT_EQUAL_INT(12, distances[vindex2(2)]);
+    ASSERT_EQUAL_INT(12, distances[vindex2(3)]);
+    ASSERT_EQUAL_INT(10, distances[vindex2(4)]);
+    ASSERT_EQUAL_INT(11, distances[vindex2(5)]);
+    ASSERT_EQUAL_INT(8, distances[vindex2(6)]);
+    ASSERT_EQUAL_INT(2, distances[vindex2(7)]);
+    ASSERT_EQUAL_INT(4, distances[vindex2(8)]);
+    ASSERT_EQUAL_INT(0, distances[vindex2(9)]);
+
+    ASSERT_EQUAL_INT(vindex2(4), prevs[vindex2(1)]);
+    ASSERT_EQUAL_INT(vindex2(5), prevs[vindex2(2)]);
+    ASSERT_EQUAL_INT(vindex2(5), prevs[vindex2(3)]);
+    ASSERT_EQUAL_INT(vindex2(6), prevs[vindex2(4)]);
+    ASSERT_EQUAL_INT(vindex2(7), prevs[vindex2(5)]);
+    ASSERT_EQUAL_INT(vindex2(8), prevs[vindex2(6)]);
+    ASSERT_EQUAL_INT(vindex2(9), prevs[vindex2(7)]);
+    ASSERT_EQUAL_INT(vindex2(9), prevs[vindex2(8)]);
+    ASSERT_EQUAL_INT(vindex2(9), prevs[vindex2(9)]); 
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_dijkstra(graphid2, vindex2(3), distances, prevs));
+    ASSERT_EQUAL_INT(4,  distances[vindex2(1)]);
+    ASSERT_EQUAL_INT(2, distances[vindex2(2)]);
+    ASSERT_EQUAL_INT(0, distances[vindex2(3)]);
+    ASSERT_EQUAL_INT(9, distances[vindex2(4)]);
+    ASSERT_EQUAL_INT(1, distances[vindex2(5)]);
+    ASSERT_EQUAL_INT(11, distances[vindex2(6)]);
+    ASSERT_EQUAL_INT(10, distances[vindex2(7)]);
+    ASSERT_EQUAL_INT(8, distances[vindex2(8)]);
+    ASSERT_EQUAL_INT(12, distances[vindex2(9)]);
+
+    ASSERT_EQUAL_INT(vindex2(3), prevs[vindex2(1)]);
+    ASSERT_EQUAL_INT(vindex2(5), prevs[vindex2(2)]);
+    ASSERT_EQUAL_INT(vindex2(3), prevs[vindex2(3)]);
+    ASSERT_EQUAL_INT(vindex2(1), prevs[vindex2(4)]);
+    ASSERT_EQUAL_INT(vindex2(3), prevs[vindex2(5)]);
+    ASSERT_EQUAL_INT(vindex2(4), prevs[vindex2(6)]);
+    ASSERT_EQUAL_INT(vindex2(5), prevs[vindex2(7)]);
+    ASSERT_EQUAL_INT(vindex2(5), prevs[vindex2(8)]);
+    ASSERT_EQUAL_INT(vindex2(8), prevs[vindex2(9)]);     
+
+    ASSERT_EQUAL_INT(e_graph_success, graph_free(graphid1));
+    ASSERT_EQUAL_INT(e_graph_success, graph_free(graphid2));
+}
+
 static void _graph_test_cases(void)
 {
     _graph_test_case01();
     _graph_test_case02();
+    _graph_test_case03();
 }
 
 int main(int argc, char *argv[])
